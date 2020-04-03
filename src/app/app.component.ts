b892d36984e35565f294312384b38463389a74f6
cadicmathieu <cadicmathieu@gmail.com>
2020-04-03 22:55:17 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'musicBook';
+  title = "musicBook";
+
+  columnDefs = [
+    { headerName: "Make", field: "make", sortable: true },
+    { headerName: "Model", field: "model", sortable: true },
+    { headerName: "Price", field: "price", sortable: true }
+  ];
+
+  rowData = [
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxter", price: 72000 }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AgGridModule } from 'ag-grid-angular';
 
 import { AppComponent } from './app.component';
+import { LibraryComponent } from './library/library.component';
+import { ArtistsComponent } from './library/artists/artists.component';
+import { AlbumsComponent } from './library/albums/albums.component';
+import { TunesComponent } from './library/tunes/tunes.component';
+import { PlaylistsComponent } from './favorites/playlists/playlists.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { FavArtistsComponent } from './favorites/fav-artists/fav-artists.component';
+import { FavAlbumsComponent } from './favorites/fav-albums/fav-albums.component';
+import { ProjectsComponent } from './library/projects/projects.component';
+import { WatchListComponent } from './library/watch-list/watch-list.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'library', component: LibraryComponent },
+  { path: 'albums', component: AlbumsComponent },
+  { path: 'artists', component: ArtistsComponent },
+  { path: 'allsongs', component: TunesComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'watchlist', component: WatchListComponent },
+  //{ path: '**', component: PageNotFoundComponent },
+
+  //{ path: 'path/:routeParam', component: MyComponent },
+  //{ path: 'staticPath', component: ... },
+  //{ path: '**', component: ... },
+  //{ path: 'oldPath', redirectTo: '/staticPath' },
+  //{ path: ..., component: ..., data: { message: 'Custom' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibraryComponent,
+    ArtistsComponent,
+    AlbumsComponent,
+    TunesComponent,
+    PlaylistsComponent,
+    FavoritesComponent,
+    FavArtistsComponent,
+    FavAlbumsComponent,
+    ProjectsComponent,
+    WatchListComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
